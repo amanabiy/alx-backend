aab7d6e7083ee001c944c12c3a6a49afc29c4ebd
Amanuel Sisay <amanuelsisay.as@gmail.com>
2021-12-22 13:49:42 +0300

    correct style and logic error on put

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Basic Cache module
+Basic Cache module 
+with put and get method
 """
 BaseCaching = __import__('base_caching').BaseCaching
 
@@ -8,19 +9,21 @@ BaseCaching = __import__('base_caching').BaseCaching
 class BasicCache(BaseCaching):
     """ A baseic cache class
     with a get and put method """
+
     def __init__(self):
         """ instantiating from the base class """
         super().__init__()
+
     def put(self, key, item):
         """ puts an item to the caching dictionary """
-        if key or item:
+        if key and item:
             self.cache_data[key] = item
         else:
             pass
+
     def get(self, key):
         """ get an item with the key """
         if key and key in self.cache_data:
             return self.cache_data[key]
         else:
             return None
-    
