2ce3c30e521f1b2d9ae18c101ba1223fa0a358b9
Amanuel Sisay <amanuelsisay.as@gmail.com>
2021-12-22 11:34:21 +0300

    implemented get and put methods

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Basic Cache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    """ A baseic cache class
+    with a get and put method """
+    def __init__(self):
+        """ instantiating from the base class """
+        super().__init__()
+    def put(self, key, item):
+        """ puts an item to the caching dictionary """
+        if key or item:
+            self.cache_data[key] = item
+        else:
+            pass
+    def get(self, key):
+        """ get an item with the key """
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
+    
