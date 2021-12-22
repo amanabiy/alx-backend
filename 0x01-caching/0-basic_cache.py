141eb9ae36b136dfe703ffbb4307b074dc3baf60
Amanuel Sisay <amanuelsisay.as@gmail.com>
2021-12-22 13:44:10 +0300

    executable file

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Basic Cache module
 """
@@ -8,21 +8,19 @@ BaseCaching = __import__('base_caching').BaseCaching
 class BasicCache(BaseCaching):
     """ A baseic cache class
     with a get and put method """
-
     def __init__(self):
         """ instantiating from the base class """
         super().__init__()
-
     def put(self, key, item):
         """ puts an item to the caching dictionary """
         if key or item:
             self.cache_data[key] = item
         else:
             pass
-
     def get(self, key):
         """ get an item with the key """
         if key and key in self.cache_data:
             return self.cache_data[key]
         else:
             return None
+    
