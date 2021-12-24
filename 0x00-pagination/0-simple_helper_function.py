7ec2422132dac4eedfb82d07b81960cae32d1482
Amanuel Sisay <amanuelsisay.as@gmail.com>
2021-12-24 21:05:45 +0300

    start index formula correctted

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,6 +6,6 @@ Find the index range
 
 def index_range(page: int, page_size: int) -> tuple:
     """ return a start index and an end index corresponding to the range """
-    start = 0 if page == 1 else page
+    start = sum([page_size for i in range(page - 1)]) if page > 0 else 0
     end = page_size + start
     return (start, end)
