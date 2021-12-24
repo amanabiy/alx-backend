6c362bcd701a2bdad001b69ef99819e917307d71
Amanuel Sisay <amanuelsisay.as@gmail.com>
2021-12-24 20:59:48 +0300

    correct end page indexing

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,5 +7,5 @@ Find the index range
 def index_range(page: int, page_size: int) -> tuple:
     """ return a start index and an end index corresponding to the range """
     start = 0 if page == 1 else page
-    end = page + start
+    end = page_size + start
     return (start, end)
