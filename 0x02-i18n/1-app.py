35a81d7830b483fbabaa9f301c0159e9e099fe6c
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-04 23:56:57 +0300

    fix run time error

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,28 +1,30 @@
+
 #!/usr/bin/env python3
 """
-Flask app setup
-use bable
+Basic Falsk app module
 """
 from flask import Flask, render_template
-from flask_babel import Bable
-
-
-class Config:
-    """ languages class """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
-    BABEL_DEFAULT_TIMEZONE = LANGUAGES[1]
+from flask_babel import Babel
 
 app = Flask(__name__)
-bable = Bable(app)
-app.config.from_object('Config')
+babel = Babel(app)
 
 
-@app.route("/")
-def home():
-    """ display home route"""
-    return render_template('0-index.html')
+class Config(object):
+    """ Language and timezone configuration"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-if __name__ == '__main__':
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello():
+    """ home route """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
