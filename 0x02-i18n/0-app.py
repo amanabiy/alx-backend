7f70376a53a1c006ef0bf5d413b3b418719ee7e4
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-04 22:41:21 +0300

    setup flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,9 +4,11 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 @app.route("/")
 def home():
     return render_template('0-index.html')
 
+
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
