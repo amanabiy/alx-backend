8a839c4bc520647af2658701248fa814580d6f84
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-04 23:06:35 +0300

    configure babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Flask app setup
+use bable
+"""
+from flask import Flask, render_template
+from flask_babel import Bable
+
+
+class Config:
+    """ languages class """
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config = Config()
+# app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[0]
+# app.config['BABEL_DEFAULT_TIMEZONE'] = Config.LANGUAGES[1]
+bable = Bable(app)
+
+
+@app.route("/")
+def home():
+    """ display home route"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
