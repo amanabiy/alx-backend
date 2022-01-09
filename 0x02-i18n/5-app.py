f96ad7a86f48d76476515ada7dec23bb4abf686a
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-09 23:32:45 +0300

    added user login functionality

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,12 +2,19 @@
 """
 Basic Falsk app module
 """
-from flask import Flask, render_template, request
+from typing import Optional
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 class Config(object):
     """ Language and timezone configuration"""
@@ -23,7 +30,7 @@ app.config.from_object(Config)
 def hello():
     """home route
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 @babel.localeselector
@@ -36,6 +43,17 @@ def get_locale():
         return local
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+def get_user():
+    """ simulate loggining in """
+    try:
+        login_as = request.args.get('login_as')
+        return users[int(login_as)]
+    except Exception:
+        return None
+
+@app.before_request
+def before_request():
+    g.user = get_user()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
