a091d0db6af6257b2ecb8aea3d92240d008d777f
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-09 23:35:14 +0300

    fixed pycodestyle

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -8,13 +8,13 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
-
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
 
 class Config(object):
     """ Language and timezone configuration"""
@@ -43,6 +43,7 @@ def get_locale():
         return local
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 def get_user():
     """ simulate loggining in """
     try:
@@ -51,9 +52,12 @@ def get_user():
     except Exception:
         return None
 
+
 @app.before_request
 def before_request():
+    """ set users in global variable before request """
     g.user = get_user()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
