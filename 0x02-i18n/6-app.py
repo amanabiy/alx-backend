f9ec5853c92a3d67f92ba8197d04bb16d09f2458
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-10 00:12:11 +0300

    get the language from different sources in heirarchy

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+Basic Falsk app module
+"""
+from typing import Optional
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """ Language and timezone configuration"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello():
+    """home route
+    """
+    return render_template('5-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get the best match language
+    """
+    local = request.args.get('locale')
+    if local and local in app.config['LANGUAGES']:
+        return local
+    elif g.user != None and g.user['locale'] in app.config['LANGUAGES']:
+        return g.user['locale']
+    local = request.headers.get('locale')
+    if local in app.config['LANGUAGES']:
+        return local
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """ simulate loggining in """
+    try:
+        login_as = request.args.get('login_as')
+        return users[int(login_as)]
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """ set users in global variable before request """
+    g.user = get_user()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
