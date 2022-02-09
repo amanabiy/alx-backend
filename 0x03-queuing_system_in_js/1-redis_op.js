8c60064aec9a90245db01c382634556da1f9602b
Amanuel Sisay <amanuelabiy.as@gmail.com>
2022-02-09 21:53:12 +0300

    queue

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,22 @@
+import {createClient} from 'redis'
+import redis from 'redis'
+
+const client = createClient();
+client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print)  
+}
+
+function displaySchoolValue(schoolName) {
+  const value = client.get(schoolName, (err, res) => {
+    if (err) console.log(err)
+    else console.log(res)
+  });
+}
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
